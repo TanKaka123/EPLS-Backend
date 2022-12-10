7f7b22180d004c36155e39ea1b3ed241af17a268
Hong-Tan <hongtan1422002@sgmail.com>
2022-12-10 12:52:16 +0700

    change route

--- Routes/account.js
+++ Routes/account.js
@@ -2,9 +2,9 @@ const express = require('express');
 const account = require('../Controllers/account/account');
 
 const router = express.Router();
-router.post('/account', account.create);
+router.post('/_account', account.create);
 
-router.get('/account',account.getAll);
+router.get('/_account',account.getAll);
 //router.get('/account/:id', account.getOne);
 
 module.exports = router;
--- Routes/post.js
+++ Routes/post.js
@@ -2,9 +2,9 @@ const express = require('express');
 const post = require('../Controllers/post/post');
 
 const router = express.Router();
-router.post('/post', post.create);
+router.post('/_post', post.create);
 
-router.get('/post',post.getAll);
+router.get('/_post',post.getAll);
 //router.get('/post/:id', post.getOne);
 
 module.exports = router;
